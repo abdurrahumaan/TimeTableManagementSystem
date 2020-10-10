f8fc83bb587752610b0b094f48abf5b96560afca
shalomiFernando <54903602+shalomiFernando@users.noreply.github.com>
2020-10-10 09:23:15 +0530

    controller added

--- SLIIT Assignment/SLIIT Assignment/src/Controllers/Student/StudentController.java
+++ SLIIT Assignment/SLIIT Assignment/src/Controllers/Student/StudentController.java
@@ -13,7 +13,6 @@ import Models.Semester;
 import Models.Student;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.regex.Pattern;
 
 /**
  *
@@ -24,15 +23,6 @@ public class StudentController {
     FacultyController facultyController;
     SemesterController semesterController;
     ProgrammeController programmeController;
-
-    int totStudentCount;
-    int firstYearStudentCount;
-    int secondYearStudentCount;
-    int thirdYearStudentCount;
-    int fourthYearStudentCount;
-    int firstProgrammeStudentCount;
-    int seconProgrammeStudentCount;
-    int thirdProgrammeStudentCount;
 
     public StudentController() {
         facultyController = new FacultyController();
@@ -69,7 +59,8 @@ public class StudentController {
         try {
             ResultSet rs = DB_Connection.search("SELECT * FROM student WHERE status=1");
             while (rs.next()) {
-                arrayList.add(new Student(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), facultyController.searchFaculty(rs.getInt(7)), semesterController.serachSemester(rs.getInt(8)), programmeController.serachProgramme(rs.getInt(9)), rs.getInt(10)));
+                arrayList.add(new Student(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), facultyController.searchFaculty(rs.getInt(7)), semesterController.serachSemester(rs.getInt(8)), programmeController.serachProgramme(rs.getInt(9)), rs.getInt(10)));
+//                arrayList.add(new Student(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), facultyController.searchFaculty(rs.getInt(7)), null, null, rs.getInt(10)));
 
             }
 
@@ -78,117 +69,6 @@ public class StudentController {
         }
         return arrayList;
 
-    }
-
-    public int updateStudent(Models.Student student) {
-
-        int tempReturn = 0;
-
-        try {
-
-            String query = "UPDATE student SET "
-                    + "academic_year = '" + student.getAcademic_year() + "',"
-                    + "group_no = '" + student.getGroup_no() + "',"
-                    + "sub_group_no = '" + student.getSub_group_no() + "',"
-                    + "group_id = '" + student.getGroup_id() + "',"
-                    + "sub_group_id = '" + student.getSub_group_id() + "',"
-                    + "faculty_id = '" + student.getFaculty().getFaculty_id() + "',"
-                    + "semester_id = '" + student.getSemester().getSemester_id() + "',"
-                    + "programme_id = '" + student.getProgramme().getProgramme_id() + "' "
-                    + "WHERE student_id = " + student.getStudent_id() + "";
-
-            System.out.println(query);
-
-            tempReturn = DB_Connection.iud(query);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return tempReturn;
-
-    }
-
-    public int deleteStudent(Models.Student student) {
-
-        int tempReturn = 0;
-
-        try {
-
-            String query = "UPDATE student SET "
-                    + "status = '" + student.getStatus_id() + "' "
-                    + "WHERE student_id = " + student.getStudent_id() + "";
-
-            System.out.println(query);
-
-            tempReturn = DB_Connection.iud(query);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return tempReturn;
-
-    }
-
-    public String totStudentCount() {
-
-        try {
-            ResultSet rs = DB_Connection.search("SELECT * FROM student WHERE status=1");
-            while (rs.next()) {
-
-                totStudentCount++;
-
-                if (rs.getString(5).split(Pattern.quote("."))[0].equals("Y1")) {
-                    firstYearStudentCount += 1;
-                } else if (rs.getString(5).split(Pattern.quote("."))[0].equals("Y2")) {
-                    secondYearStudentCount += 1;
-                } else if (rs.getString(5).split(Pattern.quote("."))[0].equals("Y3")) {
-                    thirdYearStudentCount += 1;
-                } else if (rs.getString(5).split(Pattern.quote("."))[0].equals("Y4")) {
-                    fourthYearStudentCount += 1;
-                }
-
-                if (rs.getString(5).split(Pattern.quote("."))[2].equals("IT")) {
-                    firstProgrammeStudentCount += 1;
-                } else if (rs.getString(5).split(Pattern.quote("."))[2].equals("CSSE")) {
-                    seconProgrammeStudentCount += 1;
-                } else if (rs.getString(5).split(Pattern.quote("."))[2].equals("CSE")) {
-                    thirdProgrammeStudentCount += 1;
-                }
-
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        String tempReturn = firstYearStudentCount + "-" + secondYearStudentCount + "-" + thirdYearStudentCount + "-" + fourthYearStudentCount + "-" + firstProgrammeStudentCount + "-" + seconProgrammeStudentCount + "-" + thirdProgrammeStudentCount + "-" + totStudentCount;
-
-        System.out.println(tempReturn);
-
-        firstYearStudentCount = 0;
-        secondYearStudentCount = 0;
-        thirdYearStudentCount = 0;
-        fourthYearStudentCount = 0;
-        firstProgrammeStudentCount = 0;
-        seconProgrammeStudentCount = 0;
-        thirdProgrammeStudentCount = 0;
-
-        return tempReturn;
-
-    }
-
-    public Student getStudent(int id) {
-        Student tag = null;
-        try {
-            ResultSet rs = DB_Connection.search("SELECT * FROM student WHERE student_id=" + id);
-            if (rs.first()) {
-                tag = new Student(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), facultyController.searchFaculty(rs.getInt(7)), semesterController.serachSemester(rs.getInt(8)), programmeController.serachProgramme(rs.getInt(9)), rs.getInt(10));
-            }
-        } catch (Exception e) {
-        }
-        return tag;
     }
 
 }
